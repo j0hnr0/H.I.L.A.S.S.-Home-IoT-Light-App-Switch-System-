1f5ad5f62803eb4d1baefc2c8c67fdc8c06604d1
Rovan <johnrovanccabucos@gmail.com>
2022-12-07 16:41:19 +0800

    Login Page XML Done Version 1.0

--- app/src/main/java/com/example/hilass/MainActivity.kt
+++ app/src/main/java/com/example/hilass/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.hilass
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (supportActionBar != null) {
-            supportActionBar!!.hide()
+        btnLoginStartPage.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+            }
         }
     }
 }
